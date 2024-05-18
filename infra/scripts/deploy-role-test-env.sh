9dd323109905196d83012c1b95a2e038835d20e8
Douglas Naphas <douglasnaphas@gmail.com>
2024-05-18 09:18:07 -0400

    Put region specifications in the right script

--- infra/scripts/deploy-role-test-env.sh
+++ infra/scripts/deploy-role-test-env.sh
@@ -3,4 +3,4 @@
 set -e
 [[ ! -z ${GITHUB_REPOSITORY} ]]
 STACKNAME=github-role-$(echo ${GITHUB_REPOSITORY} | sed 's/[^a-zA-Z0-9-]/-/g')-test-env;
-npx cdk --region=us-east-2 deploy ${STACKNAME};
+AWS_REGION=us-east-2 CDK_DEFAULT_REGION=us-east-2 npx cdk --region=us-east-2 deploy ${STACKNAME};
--- infra/scripts/deploy-webapp.sh
+++ infra/scripts/deploy-webapp.sh
@@ -2,4 +2,4 @@
 
 set -e
 STACKNAME=$(npx @cdk-turnkey/stackname@2.1.0 --suffix anagrampoems-web --hash 5);
-npx cdk deploy --require-approval never ${STACKNAME};
+AWS_REGION=us-east-2 CDK_DEFAULT_REGION=us-east-2 npx cdk deploy --require-approval never ${STACKNAME};
