59e10713ebfd7d72d1800869cc8077c067a25e7c
Douglas Naphas <douglasnaphas@gmail.com>
2024-05-18 09:08:30 -0400

    Deploy the web app to us-east-2

--- infra/scripts/deploy-role-test-env.sh
+++ infra/scripts/deploy-role-test-env.sh
@@ -3,4 +3,4 @@
 set -e
 [[ ! -z ${GITHUB_REPOSITORY} ]]
 STACKNAME=github-role-$(echo ${GITHUB_REPOSITORY} | sed 's/[^a-zA-Z0-9-]/-/g')-test-env;
-npx cdk deploy ${STACKNAME};
+npx cdk --region=us-east-2 deploy ${STACKNAME};
