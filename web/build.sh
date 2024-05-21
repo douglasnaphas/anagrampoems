9307e1b25f115ae41d7a2b1058029f509d0112cf
Douglas Naphas <douglasnaphas@gmail.com>
2024-05-21 00:03:33 -0400

    Copy node_modules into the Lambda artifact

--- web/build.sh
+++ web/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
+rm -rf node_modules
+npm install
 npx tsc --project tsconfig.front.json
 npx vite build
 npx tsc --project tsconfig.back.json
 cp -R dist build/
+cp -R node_modules build/
