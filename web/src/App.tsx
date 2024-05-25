f54a82a78319c7f2f82ca6c47a7a996911b99b4e
Douglas Naphas <douglasnaphas@gmail.com>
2024-05-25 08:41:33 -0400

    Try to get svg importing to work
    My web Lambda function is failing with:
    
    "errorMessage": "Error: Cannot find module './assets/react.svg'\nRequire
    stack:\n- /var/task/src/App.js\n- /var/task/src/entry-server.js\n-
    /var/task/express-app.js\n- /var/task/lambda.js\n-
    /var/runtime/index.mjs",

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
