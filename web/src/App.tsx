b8b8f8896c409a2d52dd21dbbe231b390ab202f2
Douglas Naphas <douglasnaphas@gmail.com>
2024-05-28 00:10:51 -0400

    Still try to import the logos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 import { useState } from 'react'
-// import reactLogo from './react.svg'
-// import viteLogo from '/vite.svg'
+import reactLogo from './react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
 
