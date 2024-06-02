2ce5373a821052387698d254f5923860ebc8ffd0
Douglas Naphas <douglasnaphas@gmail.com>
2024-06-02 09:15:23 -0400

    Provide a route for /

--- backend/express-app.js
+++ backend/express-app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("/ from Express on AWS Lambda!");
+});
+
 app.get("/hello", (req, res) => {
   res.send("Hello from Express on AWS Lambda!");
 });
