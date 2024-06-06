8baff3f8ce0385159832bbd01235204aa43dbd3e
Douglas Naphas <douglasnaphas@gmail.com>
2024-06-05 21:27:56 -0400

    Return the sends
    I'm not sure why, but hitting my endpoint is firing the function, but
    not giving me a web response.

--- backend/express-app.js
+++ backend/express-app.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("/ from Express on AWS Lambda!");
+  return res.send("/ from Express on AWS Lambda!");
 });
 
 app.get("/hello", (req, res) => {
-  res.send("Hello from Express on AWS Lambda!");
+  return res.send("Hello from Express on AWS Lambda!");
 });
 
 module.exports = app;
