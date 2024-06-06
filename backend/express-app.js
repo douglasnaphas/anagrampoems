19b30e077fce44136434e1d2ad0f7986d23b8025
Douglas Naphas <douglasnaphas@gmail.com>
2024-06-05 22:58:04 -0400

    Log all requests at backend
    The paths aren't lining up

--- backend/express-app.js
+++ backend/express-app.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.use("*", (req, res, next) => {
+  console.log("received a request");
+  console.log(req);
+  return next();
+});
+
 app.get("/", (req, res) => {
   return res.send("/ from Express on AWS Lambda!");
 });
