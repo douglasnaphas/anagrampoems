413fad8e7bbad0e73760e17d80a5f2d6d9e8443e
Douglas Naphas <douglasnaphas@gmail.com>
2024-06-05 21:57:27 -0400

    Set a default integration, so all paths work

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -11,20 +11,15 @@ import path = require("path");
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // backend
     const webFn = new lambda.Function(this, "WebFn", {
       code: lambda.Code.fromAsset("../backend"),
       handler: "lambda.handler",
       runtime: lambda.Runtime.NODEJS_20_X,
     });
-    const defaultApiIntegration = new HttpLambdaIntegration(
-      "WebIntegration",
-      webFn
-    );
-    const api = new apigwv2.HttpApi(this, "API");
-    api.addRoutes({
-      path: "/",
-      methods: [apigwv2.HttpMethod.ANY],
-      integration: defaultApiIntegration,
+    const api = new apigwv2.HttpApi(this, "API", {
+      defaultIntegration: new HttpLambdaIntegration("WebIntegration", webFn),
     });
     const apiHostName =
       api.apiId + ".execute-api." + this.region + "." + this.urlSuffix;
