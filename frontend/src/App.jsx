26359801f5bcfa7c6b580527fba627ee52ca2f25
Douglas Naphas <douglasnaphas@gmail.com>
2024-06-18 07:16:51 -0400

    Try to display two centered grid items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,13 @@ import { useState } from "react";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import './App.css';
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Anagram Poems</h1>
-      <Box
-        component="form"
-        noValidate
-        autoComplete="off"
-      >
+      <Box component="form" noValidate autoComplete="off">
         <TextField
           id="standard-basic"
           label="Enter a name, word, or phrase"
@@ -21,6 +16,14 @@ function App() {
           className="custom-textfield"
         />
       </Box>
+      <Grid container>
+        <Grid item xs={6}>
+          <h2>Lines</h2>
+        </Grid>
+        <Grid item xs={6}>
+          <h2>Dictionary</h2>
+        </Grid>
+      </Grid>
     </>
   );
 }
