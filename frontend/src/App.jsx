77dd626020ee93f3fc829f5075d8d3cc9e83ba51
Douglas Naphas <douglasnaphas@gmail.com>
2024-06-22 17:11:56 -0400

    Add a _Bust grams_ (submit) button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
 import "./App.css";
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
           variant="standard"
           className="custom-textfield"
         />
+        <div>
+          <Button>Bust grams</Button>
+        </div>
       </Box>
       <Grid container>
         <Grid item xs={6} className="grid-item left-align">
