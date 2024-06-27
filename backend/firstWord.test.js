ff866b0d565f06eeff87f6177878865607bc4042
Douglas Naphas <douglasnaphas@gmail.com>
2024-06-27 00:28:37 -0400

    Remove renamed file firstWord.test.js
    It's just smoke.test.js now

--- backend/firstWord.test.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest");
-const app = require("./express-app");
-
-describe("Test the root path", () => {
-  test("It should response the GET method", async () => {
-    const response = await request(app).get("/backend/");
-    expect(response.statusCode).toBe(200);
-  });
-});
