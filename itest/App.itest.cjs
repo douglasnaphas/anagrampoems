1c3e9f3aa4c91b0f9342bbea9d4f4682d00a784c
Douglas Naphas <douglasnaphas@gmail.com>
2024-12-22 07:56:01 -0500

    Delete the test user after the test

--- itest/App.itest.cjs
+++ itest/App.itest.cjs
@@ -6,6 +6,7 @@ const stackname = require("@cdk-turnkey/stackname");
 const {
   CognitoIdentityProviderClient,
   AdminCreateUserCommand,
+  AdminDeleteUserCommand,
 } = require("@aws-sdk/client-cognito-identity-provider");
 const crypto = require("crypto");
 
@@ -94,102 +95,119 @@ const waitOptions = { timeout /*, visible: true */ };
       "Failed to create a user in AWS SDK v3 setup"
     );
   }
+  try {
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    // Actual test
 
-  //////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////
-  // Actual test
+    const browser = await puppeteer.launch(browserOptions);
+    browsers.push(browser);
+    const page = await browser.newPage();
+    await page.goto(site);
 
-  const browser = await puppeteer.launch(browserOptions);
-  browsers.push(browser);
-  const page = await browser.newPage();
-  await page.goto(site);
+    // Enter "kate" and bust grams
+    const thingToGramSelector = "#thing-to-gram";
+    await page.waitForSelector(thingToGramSelector);
+    const bustGramsButtonSelector = "#bust-grams";
 
-  // Enter "kate" and bust grams
-  const thingToGramSelector = "#thing-to-gram";
-  await page.waitForSelector(thingToGramSelector);
-  const bustGramsButtonSelector = "#bust-grams";
-
-  // The Bust Grams button should be disabled without login
-  const isBustGramsButtonDisabled = await page.evaluate((selector) => {
-    const button = document.querySelector(selector);
-    return button.disabled;
-  }, bustGramsButtonSelector);
-  if (!isBustGramsButtonDisabled) {
-    await failTest(
-      "Home page test error",
-      "Expected Bust Grams button to be disabled"
-    );
-  }
-
-  // There should be an explanation that login is required
-  const requiresLoginTextSelector = "#requires-login-text"; // Adjust the selector as needed
-  await page.waitForSelector(requiresLoginTextSelector);
-  const requiresLoginText = await page.evaluate((selector) => {
-    const element = document.querySelector(selector);
-    return element.textContent;
-  }, requiresLoginTextSelector);
-  if (requiresLoginText != "Requires login") {
-    await failTest(
-      "Home page test error",
-      "Expected 'Requires login' text not found"
-    );
-  }
-
-  // Check for Login button
-  const buttonsSelector = "button";
-  const loginButtonText = await page.evaluate((selector) => {
-    const buttons = Array.from(document.querySelectorAll(selector));
-    const loginButton = buttons.find(
-      (button) => button.textContent.trim() === "Login"
-    );
-    return loginButton ? loginButton.textContent : null;
-  }, buttonsSelector);
-
-  if (loginButtonText !== "Login") {
-    await failTest("Home page test error", "Expected 'Login' button not found");
-  }
-
-  // Click the Login button
-  const loginButtonSelector = "#login-button";
-  await page.waitForSelector(loginButtonSelector);
-  await Promise.all([
-    page.click(loginButtonSelector),
-    page.waitForNavigation(),
-  ]);
-  if (page.url() !== idpUrl) {
-    failTest(
-      new Error("wrong IDP URL"),
-      `expected IDP URL ${idpUrl}, got ${page.url()}`,
-      browsers
-    );
-  }
-  // Enter username
-  const usernameSelector = `input#signInFormUsername[type='text']`;
-  await page.waitForSelector(usernameSelector);
-  await page.type(usernameSelector, adminCreateUserInput.Username);
-  // Enter temp password
-  const passwordSelector = `input#signInFormPassword[type='password']`;
-  await page.type(passwordSelector, adminCreateUserInput.TemporaryPassword);
-  const submitButtonSelector = `input[name='signInSubmitButton'][type='Submit']`;
-  await page.click(submitButtonSelector);
-  // Change password
-  const password = randString({ numLetters: 8 });
-  const newPasswordSelector = `input#new_password[type='password']`;
-  await page.waitForSelector(newPasswordSelector);
-  await page.type(newPasswordSelector, password);
-  const confirmPasswordSelector = `input#confirm_password[type='password']`;
-  await page.type(confirmPasswordSelector, password);
-  const resetPassWordButtonSelector = `button[name="reset_password"][type='submit']`;
-  await page.click(resetPassWordButtonSelector);
-
-  ////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
-  // Clean up
-  for (let b = 0; b < browsers.length; b++) {
-    if (browsers[b].close) {
-      await browsers[b].close();
+    // The Bust Grams button should be disabled without login
+    const isBustGramsButtonDisabled = await page.evaluate((selector) => {
+      const button = document.querySelector(selector);
+      return button.disabled;
+    }, bustGramsButtonSelector);
+    if (!isBustGramsButtonDisabled) {
+      await failTest(
+        "Home page test error",
+        "Expected Bust Grams button to be disabled"
+      );
     }
+
+    // There should be an explanation that login is required
+    const requiresLoginTextSelector = "#requires-login-text"; // Adjust the selector as needed
+    await page.waitForSelector(requiresLoginTextSelector);
+    const requiresLoginText = await page.evaluate((selector) => {
+      const element = document.querySelector(selector);
+      return element.textContent;
+    }, requiresLoginTextSelector);
+    if (requiresLoginText != "Requires login") {
+      await failTest(
+        "Home page test error",
+        "Expected 'Requires login' text not found"
+      );
+    }
+
+    // Check for Login button
+    const buttonsSelector = "button";
+    const loginButtonText = await page.evaluate((selector) => {
+      const buttons = Array.from(document.querySelectorAll(selector));
+      const loginButton = buttons.find(
+        (button) => button.textContent.trim() === "Login"
+      );
+      return loginButton ? loginButton.textContent : null;
+    }, buttonsSelector);
+
+    if (loginButtonText !== "Login") {
+      await failTest(
+        "Home page test error",
+        "Expected 'Login' button not found"
+      );
+    }
+
+    // Click the Login button
+    const loginButtonSelector = "#login-button";
+    await page.waitForSelector(loginButtonSelector);
+    await Promise.all([
+      page.click(loginButtonSelector),
+      page.waitForNavigation(),
+    ]);
+    if (page.url() !== idpUrl) {
+      failTest(
+        new Error("wrong IDP URL"),
+        `expected IDP URL ${idpUrl}, got ${page.url()}`,
+        browsers
+      );
+    }
+    // Enter username
+    const usernameSelector = `input#signInFormUsername[type='text']`;
+    await page.waitForSelector(usernameSelector);
+    await page.type(usernameSelector, adminCreateUserInput.Username);
+    // Enter temp password
+    const passwordSelector = `input#signInFormPassword[type='password']`;
+    await page.type(passwordSelector, adminCreateUserInput.TemporaryPassword);
+    const submitButtonSelector = `input[name='signInSubmitButton'][type='Submit']`;
+    await page.click(submitButtonSelector);
+    // Change password
+    const password = randString({ numLetters: 8 });
+    const newPasswordSelector = `input#new_password[type='password']`;
+    await page.waitForSelector(newPasswordSelector);
+    await page.type(newPasswordSelector, password);
+    const confirmPasswordSelector = `input#confirm_password[type='password']`;
+    await page.type(confirmPasswordSelector, password);
+    const resetPassWordButtonSelector = `button[name="reset_password"][type='submit']`;
+    await page.click(resetPassWordButtonSelector);
+
+    ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    // Clean up
+
+    for (let b = 0; b < browsers.length; b++) {
+      if (browsers[b].close) {
+        await browsers[b].close();
+      }
+    }
+    ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  } finally {
+    // Clean up test user
+    const adminDeleteUserInput = {
+      UserPoolId: userPoolId,
+      Username: adminCreateUserInput.Username,
+    };
+    await cognitoIdentityProviderClient.send(
+      new AdminDeleteUserCommand(adminDeleteUserInput)
+    );
   }
-  ////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
 })();
