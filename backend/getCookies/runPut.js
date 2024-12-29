c5a780be15846461f5cd71dd7d282cf7e35893c9
Douglas Naphas <douglasnaphas@gmail.com>
2024-12-29 00:03:42 -0500

    Fail if writing cookie info to the db fails
    There are no items in the db

--- backend/getCookies/runPut.js
+++ backend/getCookies/runPut.js
@@ -2,16 +2,8 @@ const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
 
 /**
- * Return middleware satisfying:
- * pre: res.locals[paramsName] is set to a valid object for params to
- * DynamoDBDocumentClient.put
- * post: DynamoDBDocumentClient.put is executed with the
- * supplied params, and res.locals.dbData and res.locals.dbError are populated
- * with the data and error from the execution
  * @param {String} paramsName The name of the res.locals property where the
  * params for this put are stored
- * @return {Function} Express middleware that executes put and calls next,
- * or sends 500 if res.locals[paramsName] is not defined
  */
 function runPut(paramsName) {
   const middleware = async (req, res, next) => {
@@ -31,6 +23,8 @@ function runPut(paramsName) {
     } catch (err) {
       res.locals.dbError = err;
       res.locals.dbData = null;
+      console.log("runPut error", err);
+      return res.status(500).send(responses.SERVER_ERROR);
     }
 
     return next();
