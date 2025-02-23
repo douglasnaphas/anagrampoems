f4432f461714b4ac8cfb6dc6eb9b9dab712f75b7
Douglas Naphas <douglasnaphas@gmail.com>
2025-02-23 18:59:06 -0500

    Remove web, web-express, web-js
    I'm using just backend/ and frontend/.

--- web/lambda.ts
+++
@@ -1,7 +0,0 @@
-import serverless from "serverless-http";
-import app from "./express-app";
-import type { Handler } from "aws-lambda";
-
-const handler: Handler = serverless(app);
-
-export { handler };
